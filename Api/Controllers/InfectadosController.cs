14001482199c4d47e81c5c519d021754e17f5e91
Diego Fernandes Costa <diego.fernandes721@gmail.com>
2021-03-25 19:53:54 -0300

    projeto api

---
+++ Api/Controllers/InfectadosController.cs
@@ -0,0 +1,39 @@
+﻿using Api.Data.Collections;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectados> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectados>(typeof(Infectados).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectados(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectados>.Filter.Empty).ToList();
+
+            return Ok(infectados);
+        }
+    }
+}
---
+++ Api/Data/Collections/Infectados.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver.GeoJsonObjectModel;
+
+
+namespace Api.Data.Collections
+{
+    public class Infectados
+    {
+        public Infectados (DateTime dataNascimento, string sexo, double latitude, double longitude)
+        {
+            DataNascimento = dataNascimento;
+            Sexo = sexo;
+            Localizacao = new GeoJson2DGeographicCoordinates(longitude, latitude);
+
+        }
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; }
+    }
+}
